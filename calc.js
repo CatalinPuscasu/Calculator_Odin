500d73ac31b852de0c1af7114531e5620c967c40
CatalinPuscasu <puscasucatalin437@gmail.com>
2022-01-23 00:07:10 +0200

    Added the basic math functions for the project

---
+++ calc.js
@@ -0,0 +1,25 @@
+//math operations
+
+function add(a, b) {
+    return a + b;
+}
+
+console.log(add(10, 45));
+
+function subtract(a, b) {
+    return a - b;
+}
+
+console.log(subtract(43, 8));
+
+function multiply(a, b) {
+    return a * b;
+}
+
+console.log(multiply(2, 9));
+
+function divide(a, b) {
+    return a / b;
+}
+
+console.log(divide(48, 2));
